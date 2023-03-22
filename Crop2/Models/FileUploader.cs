f5c53784a5d79ddd275b2fc771dc9f44f37cb1e4
hoztekin <hoztekin81@hotmail.com>
2023-03-23 00:07:15 +0300

    Add

--- Crop2/Models/FileUploader.cs
+++ Crop2/Models/FileUploader.cs
@@ -6,23 +6,35 @@ namespace Crop2.Models
     {
         public  bool Resize(int width, int height, string source_path)
         {
+
             if (!File.Exists(source_path))
             {
                 return false;
             }
-            var local_image_dir = $"{source_path}";
-            var file_path = Path.GetDirectoryName(source_path) ;
-            if (!Directory.Exists(Path.Combine(file_path)))
-                Directory.CreateDirectory(Path.Combine(file_path));
-
-
-            using (var image = new MagickImage(source_path))
+            try
             {
-                image.Resize(width, height);
-                image.Write(source_path);
+                //var local_image_dir = $"{source_path}";
+                var file_path = Path.GetDirectoryName(source_path);
+                if (!Directory.Exists(Path.Combine(file_path)))
+                    Directory.CreateDirectory(Path.Combine(file_path));
+
+
+                using (var image = new MagickImage(source_path))
+                {
+
+                    image.Resize(width, height);
+                    image.Write(source_path);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
             }
 
-            return true;
+           
+            
         }
     }
 }

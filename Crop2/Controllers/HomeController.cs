d1691d2047f6c316cd39b37dd789d1d03c5a5e0e
hoztekin <hoztekin81@hotmail.com>
2023-03-23 00:06:59 +0300

    Add

---
+++ Crop2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Crop2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Crop2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var image_helper = new FileUploader();
+            var local_image_dir = $"wwwroot/_uploads/images/7324e230-91a6-4678-adb9-f790fd1607e5.png";
+            var base_64_result = image_helper.Resize(1000, 750, local_image_dir);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Crop2/Models/FileUploader.cs
@@ -0,0 +1,28 @@
+﻿using ImageMagick;
+
+namespace Crop2.Models
+{
+    public class FileUploader
+    {
+        public  bool Resize(int width, int height, string source_path)
+        {
+            if (!File.Exists(source_path))
+            {
+                return false;
+            }
+            var local_image_dir = $"{source_path}";
+            var file_path = Path.GetDirectoryName(source_path) ;
+            if (!Directory.Exists(Path.Combine(file_path)))
+                Directory.CreateDirectory(Path.Combine(file_path));
+
+
+            using (var image = new MagickImage(source_path))
+            {
+                image.Resize(width, height);
+                image.Write(source_path);
+            }
+
+            return true;
+        }
+    }
+}
